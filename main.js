b9c535634b358a175cbecda4b74b65be5a911ee6
lane <huulane@gmail.com>
2020-02-19 20:38:57 +0800

    Optimize code structure

--- main.js
+++ main.js
@@ -1,25 +1,48 @@
-import { log, imgsFromPath } from './utils.js'
-
+import { log } from './utils.js'
 import Game from './class/game.js'
 
-const main = async () => {
+const zip = function * (a, b) {
+  // Note: specially in my case that a: Iterator b: Array
+  for (const [i, value] of Array.from(a).entries()) {
+    yield [value, b[i]]
+  }
+}
+
+const imgFromPath = path => {
+  // To transform a path into Promise
+  const img = new Image()
+  img.src = path
+  return new Promise(res => {
+    img.onload = () => res(img)
+  })
+}
+
+const imgsFromPath = async paths => {
+  // convert a plain object into an ES6 Map
+  const pathMap = new Map(Object.entries(paths))
+  // loads all imgs
+  const loadedImgs = await Promise.all(
+    Array.from(pathMap.values(), p => imgFromPath(p))
+  )
+  return Object.fromEntries(zip(pathMap.keys(), loadedImgs))
+}
+
+;(async function main () {
   // load imgs
   const imgs = await imgsFromPath({
-    background: 'background',
-    clouds: 'clouds',
-    'clouds-transparent': 'clouds-transparent',
-    'enemy-big': 'enemy-big',
-    'enemy-medium': 'enemy-medium',
-    'enemy-small': 'enemy-small',
-    explosion: 'explosion',
-    bolts: 'bolts',
-    ship: 'ship',
+    background: 'imgs/background.png',
+    clouds: 'imgs/clouds.png',
+    'clouds-transparent': 'imgs/clouds-transparent.png',
+    'enemy-big': 'imgs/enemy-big.png',
+    'enemy-medium': 'imgs/enemy-medium.png',
+    'enemy-small': 'imgs/enemy-small.png',
+    explosion: 'imgs/explosion.png',
+    bolts: 'imgs/bolts.png',
+    ship: 'imgs/ship.png'
   })
 
   log('imgs:', imgs)
   window.imgs = imgs
   // run the game
   Game.new()
-}
-
-main()
+})()
--- utils.js
+++ utils.js
@@ -1,36 +1,7 @@
 const log = console.log.bind(console)
 
-const resolveObj = obj => {
-  // To resolve an obj into two arrs
-  const ks = []
-  const vs = []
-  for (const [k, v] of Object.entries(obj)) {
-    ks.push(k)
-    vs.push(v)
-  }
-  return [ks, vs]
-}
-
 const clearObj = obj => Object.keys(obj).forEach(prop => delete obj[prop])
-
-const imgFromPath = path => {
-  // To transform a path into Promise
-  const img = new Image()
-  img.src = `imgs/${path}.png`
-  return new Promise(resolve => {
-    img.onload = () => {
-      resolve(img)
-    }
-  })
-}
-
-const imgsFromPath = async paths => {
-  const [keys, vs] = resolveObj(paths)
-  // loads all imgs
-  const values = await Promise.all(vs.map(v => imgFromPath(v)))
-  return Object.fromEntries(keys.map((_, i) => [keys[i], values[i]]))
-}
 
 const type = e => e.constructor.name
 
-export { log, type, imgsFromPath, clearObj }
+export { log, type, clearObj }
